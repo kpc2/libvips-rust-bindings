d674732cfe9348acafcd9e32dfba8fd39325f4ac
apokaliptis <apokaliptis@zoho.com>
2022-12-29 22:17:49 -0500

    fix: build on windows

--- build.rs
+++ build.rs
@@ -3,4 +3,5 @@ fn main() {
     println!("cargo:rustc-link-lib=vips");
     println!("cargo:rustc-link-lib=glib-2.0");
     println!("cargo:rustc-link-lib=gobject-2.0");
+    println!("cargo:rustc-link-search=native=D:/KC/programs/scoop/apps/libvips/8.10.6/lib");
 }
